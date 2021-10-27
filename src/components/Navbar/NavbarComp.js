06e64d89675539b395588e06800d2d2b1993ce30
SidZhoop <siddharth@zhoop.in>
2021-10-28 00:16:29 +0530

    zhoopVersion1

--- src/components/Navbar/NavbarComp.js
+++ src/components/Navbar/NavbarComp.js
@@ -9,64 +9,6 @@ function NavbarComp(){
 
     const handleClick = () => setClick(!click);
         return(
-            // <nav className="NavbarItems">
-            //     <h1 className="navbar-logo">ZHOOP<i class="fas fa-shopping-cart"></i></h1>
-            //     <div className="menu-icon">
-            //         <Zoom in={true}>
-            //             <Fab onClick={this.handleClick}>
-            //                 {this.state.clicked ? <CloseIcon/>:<MenuIcon/>}
-            //             </Fab>
-            //         </Zoom>
-            //     </div>
-            //     <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-            //         <Button variant="light" className="login">Login/Sign up</Button>
-            //         {MenuItems.map((item, index) => {
-            //             return(
-            //                 <li key={index}>
-            //                     <a className={item.cName} href={item.url}>
-            //                         {item.title}
-            //                     </a>
-            //                 </li>
-            //             )
-            //         })} 
-            //     </ul>
-            // </nav>
-            // <Navbar className="NavbarItems" expand="lg">
-            //     <Container>
-            //         <Navbar.Brand href="#home">ZHOOP<i class="fas fa-shopping-cart"></i></Navbar.Brand>
-            //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            //         <Navbar.Collapse id="basic-navbar-nav">
-            //             <Nav className="me-auto">
-            //                 {MenuItems.map((item, index) => {
-            //                     return(
-            //                         <li key={index}>
-            //                             <Nav.Link className={item.cName} href={item.url}>{item.title}</Nav.Link>
-            //                         </li>
-            //                     )
-            //                 })}
-            //             </Nav>
-            //         </Navbar.Collapse>
-            //     </Container>
-            // </Navbar>
-            // <nav class="navbar fixed-top navbar-expand-lg navbar-light">
-            //     <div class="container-fluid">
-            //         <a class="navbar-brand brand" href="#">ZHOOP<i class="fas fa-shopping-cart"></i></a>
-            //         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            //         <span class="navbar-toggler-icon"></span>
-            //         </button>
-            //         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            //             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            //                 {MenuItems.map((item, index) => {
-            //                     return(
-            //                         <li class="nav-item" key={index}>
-            //                             <a class="nav-link active" aria-current="page" href={item.url}>{item.title}</a>
-            //                         </li>
-            //                     )
-            //                 })}
-            //             </ul>
-            //         </div>
-            //     </div>
-            // </nav>
             <nav className="navbar fixed-top">
                 <div className="nav-container">
                     <NavLink exact to="/" className="nav-logo">
